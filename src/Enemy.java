ff1ca482471b0ee60e8896e684e94433e92da861
Eliot996 <math10t6@stud.kea.dk>
2021-10-08 09:27:53 +0200

    Added Enemy class

---
+++ src/Enemy.java
@@ -0,0 +1,12 @@
+public class Enemy {
+
+    private String name;
+    private int health;
+    private Item weapon;
+
+    public Enemy(String name, int health, Item weapon) {
+        this.name = name;
+        this.health = health;
+        this.weapon = weapon;
+    }
+}
